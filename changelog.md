42efe49d8b2794342dd45ca11d34ca482ab54f7f
Rafael Gardel Azzariti Brasil <rafaelgardel@gmail.com>
2023-09-30 05:38:20 -0300

    atualizações

---
+++ changelog.md
@@ -0,0 +1,8 @@
+# Changelog
+Todas as alterações notáveis neste projeto serão documentadas neste arquivo.
+
+## 1.0.0 - 30 Setembro 2023
+
+### FEATURE:
+    - Adição do app criado no streamlit
+    - Adição de imagens e vídeos demonstrando a utilização
--- projeto.py
+++ projeto.py
@@ -18,6 +18,7 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from enelvo.normaliser import Normaliser
 
+
 def make_model():
     '''Importação dos dados'''
     df = pd.read_csv(r'database/dataframe_final.csv')
@@ -32,19 +33,20 @@ def make_model():
     df['Comentário'] = corrige_gramatica(df['Comentário'])
 
     # Remove a pontuação dos reviews
-    df['Comentário']=df['Comentário'].apply(lambda x: remove_pontuacao(x))
+    df['Comentário'] = df['Comentário'].apply(lambda x: remove_pontuacao(x))
 
     # lematiza a coluna comentário
     # Carregando o modelo em português do Brasil do spaCy
     nlp = spacy.load("pt_core_news_sm")
-    
-    df['Comentário'] = df['Comentário'].apply(lambda x: " ".join(lemmatize_words(x, nlp)))
 
-    df.index=np.arange(df.shape[0])
+    df['Comentário'] = df['Comentário'].apply(
+        lambda x: " ".join(lemmatize_words(x, nlp)))
+
+    df.index = np.arange(df.shape[0])
 
     df['TARGET'] = df['Nota'].apply(lambda x: avalia_nota(x))
 
-    df = df[df['Comentário'].isna()==False]
+    df = df[df['Comentário'].isna() == False]
 
     df.drop_duplicates(keep='last', inplace=True, ignore_index=True)
 
@@ -52,11 +54,12 @@ def make_model():
     x = df['Comentário'].astype('str')
     y = df['TARGET']
 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=30, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.3, random_state=30, stratify=y)
 
     # instanciando e treinando o CountVectorizer
     stopWords = nltk.corpus.stopwords.words('portuguese')
-    vectorizer = CountVectorizer(analyzer = "word", stop_words=stopWords) 
+    vectorizer = CountVectorizer(analyzer="word", stop_words=stopWords)
     X_train_vectorizer = vectorizer.fit_transform(X_train)
     X_test_vectorizer = vectorizer.transform(X_test)
 
@@ -79,23 +82,26 @@ def make_model():
     print(f'\nAcurácia: {accuracy:.2f}')
     print(f'\nRelatório de Classificação:\n', report)
 
-    cross_val_score = cross_val_score(estimator=model, X=X_test_vectorizer, y=y_test, cv=5, n_jobs=-1) #y_test, y_pred
+    cross_val_score = cross_val_score(
+        estimator=model, X=X_test_vectorizer, y=y_test, cv=5, n_jobs=-1)  # y_test, y_pred
 
     print('Resultado da validação cruzada')
     print(cross_val_score, '\n')
 
-    print(f'Média do resultado da validação cruzada: {round(cross_val_score.mean(), 2)}')
+    print(
+        f'Média do resultado da validação cruzada: {round(cross_val_score.mean(), 2)}')
 
     return model, vectorizer
-    
-def classifica_nps(model, vectorizer, comentario:str) -> np.array:
+
+
+def classifica_nps(model, vectorizer, comentario: str) -> np.array:
     "Classifica o nps baseado em um ou mais comentários e retorna o resultado"
 
     if type(comentario) == str:
         comentario = np.array([comentario])
 
     comentarios_tfidf = vectorizer.transform(comentario)
-    
+
     probabilities = model.predict_proba(comentarios_tfidf)
     resultado = model.predict(comentarios_tfidf)
 
@@ -106,52 +112,57 @@ def lemmatize_words(text, nlp):
     '''Lematiza uma lista de palavras'''
 
     doc = nlp(text)
-    
+
     lemmatized_words = [token.lemma_ for token in doc]
-    
+
     return lemmatized_words
+
 
 def corrige_gramatica(coluna):
     '''Conserta abreviações, potuações e remove emojis'''
-    
+
     norm = Normaliser(tokenizer='readable', sanitize=True)
-    
+
     # Usa a função vectorize do NumPy para aplicar a normalização a todos os elementos em uma única chamada
     normalizar = np.vectorize(norm.normalise)
 
     coluna_normalizada = normalizar(coluna)
-    
+
     return coluna_normalizada
+
 
 def remove_pontuacao(review):
     '''# Remove a pontuação dos comentários'''
 
     review = str(review)
-    review = "".join([char for char in review if char not in string.punctuation])
+    review = "".join(
+        [char for char in review if char not in string.punctuation])
     return review
+
 
 def load_model():
     '''Carrega o modelo treinado'''
     model, X_train_vectorizer, vectorizer = load('models/model.joblib')
     return model, X_train_vectorizer, vectorizer
 
+
 def avalia_nota(x):
     '''Avalia a nota e retorna a classificação do NPS'''
 
-    if x >=9:
+    if x >= 9:
         classificacao = 'Promotor'
-    
-    elif x==7 or x==8:
+
+    elif x == 7 or x == 8:
         classificacao = 'Neutro'
 
     else:
         classificacao = 'Detrator'
-    
+
     return classificacao
 
 # Ative para fazer previsões em novos comentários
-# new_comments = ["Este serviço é excelente!", 
-#                 "Estou insatisfeito com o atendimento e o produto é horrível", 
+# new_comments = ["Este serviço é excelente!",
+#                 "Estou insatisfeito com o atendimento e o produto é horrível",
 #                 "Péssimo cartão! Não fui bem atendida e meu limite é baixo"
 #                 ]
 

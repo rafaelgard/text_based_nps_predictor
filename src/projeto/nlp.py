e75b4842d3c3fbea329345a8a647d90003f712cd
Rafael.Brasil <RafaelBrasil.thera@suzano.com.br>
2023-11-24 14:23:01 -0300

    Modelo rnn

--- src/projeto/nlp.py
+++ src/projeto/nlp.py
@@ -15,14 +15,38 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from enelvo.normaliser import Normaliser
+from pyod.models.ecod import ECOD
+from scipy.sparse import csr_matrix
+nltk.download('stopwords')
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import cross_val_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+
+
 
 
 class nlp_predictor:
-    def __init__(self, retrain_model):
+    def __init__(self, retrain_model=False):
         '''Classe que permite a utilização dos métodos implementados'''
         # se retrain_model=True, o modelo é retreinado,
         # caso contrário é carregado um modelo já treinado
         self.retrain_model = retrain_model
+        self.model = None
+        self.vectorizer = None
+        self.model_type = 'RNN'
+        self.initial_setup()
+
+    def initial_setup(self):
+        '''Executa as configurações iniciais da classe'''
+        self.model, self.vectorizer = self.get_model()
 
     def make_model(self):
         '''Importação dos dados'''
@@ -59,6 +83,8 @@ class nlp_predictor:
         x = df['Comentário'].astype('str')
         y = df['TARGET']
 
+        breakpoint()
+
         # separando os dados em conjunto de teste e treino
         X_train, X_test, y_train, y_test = train_test_split(
             x, y, test_size=0.3, random_state=30, stratify=y)
@@ -69,27 +95,43 @@ class nlp_predictor:
         X_train_vectorizer = vectorizer.fit_transform(X_train)
         X_test_vectorizer = vectorizer.transform(X_test)
 
+        # print('X_train_vectorizer')
+        # print(X_train_vectorizer)
+        
+        # X_train_vectorizer_sem_outiliers, y_train_sem_outiliers = self.remove_outiliers(X_train_vectorizer, y_train)
+        # breakpoint()    
+       
         # instanciando o modelo
-        model = MultinomialNB()
+        # model = MultinomialNB()
+        
+        import lightgbm as lgb
+        model = lgb.LGBMClassifier()#colsample_bytree=0.8
 
         # treinando o modelo nos dados de treinamento
-        model.fit(X_train_vectorizer, y_train)
-
+        # model.fit(X_train_vectorizer_sem_outiliers, y_train_sem_outiliers)
+        model.fit(X_train_vectorizer.toarray(), np.array(y_train))
+        y_pred = model.predict(X_test_vectorizer.toarray())
+        # accuracy=accuracy_score(y_pred, y_train_sem_outiliers)
+        accuracy=accuracy_score(np.array(y_test),y_pred)
+        print('Training-set accuracy score: {0:0.4f}'. format(accuracy))
+        # print(classification_report(y_train_sem_outiliers, y_pred))
+        print(classification_report(np.array(y_test),y_pred))
+        
         # salvando o modelo
-        dump((model, X_train_vectorizer, vectorizer), 'src/models/model.joblib')
+        # dump((model, X_train_vectorizer_sem_outiliers, vectorizer), 'src/models/new_model.joblib')
 
         # fazendo as previsões no conjunto de teste
-        y_pred = model.predict(X_test_vectorizer)
+        # y_pred = model.predict(X_test_vectorizer)
 
         # avaliando o desempenho do modelo
-        accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred)
+        # accuracy = accuracy_score(y_test, y_pred)
+        # report = classification_report(y_test, y_pred)
 
-        print(f'\nAcurácia: {accuracy:.2f}')
-        print(f'\nRelatório de Classificação:\n', report)
+        # print(f'\nAcurácia: {accuracy:.2f}')
+        # print(f'\nRelatório de Classificação:\n', report)
 
         cross_val_score_result = cross_val_score(
-            estimator=model, X=X_test_vectorizer, y=y_test, cv=5, n_jobs=-1)
+            estimator=model, X=X_test_vectorizer.toarray(), y=np.array(y_test), cv=5, n_jobs=-1)
 
         print('Resultado da validação cruzada')
         print(cross_val_score_result, '\n')
@@ -97,7 +139,94 @@ class nlp_predictor:
         print(
             f'Média do resultado da validação cruzada: {round(cross_val_score_result.mean(), 2)}\n')
 
+        breakpoint()	
         return model, vectorizer
+
+    def make_model_RNN(self):
+        '''Importação dos dados'''
+        df = pd.read_csv(r'src/database/dataframe_final.csv')
+
+        # dropa os dados nulos
+        df.dropna(inplace=True)
+
+        # transforma todos os comentários para minúsculo
+        df['Comentário'] = df['Comentário'].str.lower()
+
+        # corrige a gramática
+        df['Comentário'] = self.corrige_gramatica(df['Comentário'])
+
+        # Remove a pontuação dos reviews
+        df['Comentário'] = df['Comentário'].apply(self.remove_pontuacao)
+
+        # lematiza a coluna comentário
+        # Carregando o modelo em português do Brasil do spaCy
+        nlp = spacy.load("pt_core_news_sm")
+
+        df['Comentário'] = df['Comentário'].apply(
+            lambda x: " ".join(self.lemmatize_words(x, nlp)))
+
+        df.index = np.arange(df.shape[0])
+
+        df['TARGET'] = df['Nota'].apply(lambda x: self.avalia_nota(x))
+
+        df = df[df['Comentário'].isna() == False]
+
+        df.drop_duplicates(keep='last', inplace=True, ignore_index=True)
+
+        # Dividindo os dados em conjunto de treinamento e conjunto de teste
+        x = df['Comentário'].astype('str')
+        y = df['TARGET']
+
+        # Label Encoding for categorical target variable
+        label_encoder = LabelEncoder()
+        y_encoded = label_encoder.fit_transform(y)
+
+        # separando os dados em conjunto de teste e treino
+        X_train, X_test, y_train, y_test = train_test_split(
+            x, y_encoded, test_size=0.3, random_state=30, stratify=y_encoded)
+
+        # Tokenizing and Padding
+        max_words = 5000
+        max_len = 100
+        tokenizer = Tokenizer(num_words=max_words)
+        tokenizer.fit_on_texts(X_train)
+        X_train_seq = tokenizer.texts_to_sequences(X_train)
+        X_test_seq = tokenizer.texts_to_sequences(X_test)
+        X_train_pad = pad_sequences(X_train_seq, maxlen=max_len)
+        X_test_pad = pad_sequences(X_test_seq, maxlen=max_len)
+
+        # Build the RNN model
+        model = Sequential()
+        model.add(Embedding(max_words, 50, input_length=max_len))
+        model.add(LSTM(100))
+        model.add(Dense(len(label_encoder.classes_), activation='softmax'))
+
+        # Compile the model
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        # Train the model
+        model.fit(X_train_pad, y_train, epochs=5, batch_size=32, validation_split=0.2)
+
+        # Evaluate the model
+        y_pred = np.argmax(model.predict(X_test_pad), axis=-1)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f'Test accuracy: {accuracy:.4f}')
+
+        breakpoint()
+
+        return model, tokenizer, label_encoder
+
+
+
+   
+
+
+
+
+
+
+
+
 
     def create_wordcloud(self, column):
         '''Cria uma word cloud de uma coluna específica do dataframe'''
@@ -144,15 +273,64 @@ class nlp_predictor:
         plt.imshow(wordcloud)
         plt.savefig('images/new_wordcloud.png')
 
-    def classifica_nps(self, model, vectorizer, data) -> (np.array, np.array):
+    def remove_outiliers(self, X_train, y_train):
+
+        y_train = y_train.reset_index(drop=True)
+        print('iniciando o remove outiliers')
+        clf = ECOD()
+        clf.fit(X_train.toarray())
+        # print('predict')
+        # out = clf.predict(X_train.toarray())
+
+        X_train = X_train.toarray()
+        y_train = np.array(y_train)
+
+        out = clf.fit_predict(X_train)
+
+        df = pd.DataFrame(X_train)
+
+        df['outliers'] = out
+
+        df['index'] = df.index
+
+        df_sem_outliers = df[df['outliers']==0].copy()
+
+        df_sem_outliers.drop(['outliers'], axis=1, inplace=True)
+
+        # breakpoint()
+        y_train_sem_outliers = y_train[df_sem_outliers['index']]
+
+        X_train_sem_outliers = df_sem_outliers.to_numpy(copy=True)
+
+        del df, df_sem_outliers
+
+        # X_train_sem_outliers = csr_matrix(X_train_sem_outliers)
+
+        # breakpoint()
+
+        # print('scores')
+        # # get outlier scores
+        # y_train_scores = clf.decision_scores_  # raw outlier scores on the train data
+        # y_test_scores = clf.decision_function(X_test.toarray())  # predict raw outlier scores on test
+
+        # print('y_train_scores')
+        # print(y_train_scores)
+        # print(y_test_scores)
+        # print(y_test_scores)
+        # # breakpoint()
+        print('Finalizou remove outliers')
+        return X_train_sem_outliers, y_train_sem_outliers
+
+    def classifica_nps(self, data) -> (np.array, np.array):
         "Classifica o nps baseado em um ou mais comentários e retorna o resultado"
 
-        comentarios_tfidf = vectorizer.transform(data)
+        #todo: caso eu altere os modelos essas funções podem ter que mudar
+        comentarios_tfidf = self.vectorizer.transform(data)
 
-        probabilities = model.predict_proba(comentarios_tfidf)
-        resultado = model.predict(comentarios_tfidf)
+        probabilidades = self.model.predict_proba(comentarios_tfidf)
+        resultado = self.model.predict(comentarios_tfidf)
 
-        return resultado, probabilities
+        return resultado, probabilidades
 
     def lemmatize_words(self, text, nlp):
         '''Lematiza uma lista de palavras'''
@@ -210,10 +388,18 @@ class nlp_predictor:
         treinar o modelo ou utilizar um modelo carregado'''
 
         if self.retrain_model:
-            model, vectorizer = self.make_model()
+            if self.model_type == 'RNN':
+                model, tokenizer, label_encoder = self.make_model_RNN()
+            else:
+                model, vectorizer = self.make_model()
 
         else:
-            model, _, vectorizer = self.load_model()
+            if self.model_type == 'RNN':
+                pass
+
+            else:
+
+                model, _, vectorizer = self.load_model()
 
         return model, vectorizer
 
@@ -239,11 +425,19 @@ class nlp_predictor:
             else:
                 comentario = data
 
-            model, vectorizer = self.get_model()
-            resultado, probabilities = self.classifica_nps(
-                model, vectorizer, comentario)
+            resultado, probabilidades = self.classifica_nps(comentario)
 
-            return resultado, probabilities
+            return resultado, probabilidades
         
         else:
             ['resultado_inválido'], [0]
+
+
+nlp_obj = nlp_predictor(True)
+
+data =  ["Este serviço é excelente!",
+         "Estou insatisfeito com o atendimento e o produto é horrível",
+         "Péssimo cartão! Não fui bem atendida e meu limite é baixo"]
+
+nlp_obj.predict(data)
+breakpoint()
